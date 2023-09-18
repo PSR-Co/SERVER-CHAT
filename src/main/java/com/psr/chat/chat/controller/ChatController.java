ca9e738c28f1d230c8d56a2ed3fb26bc36bc7558
sojungpp <sophie2458@naver.com>
2023-09-19 00:09:39 +0900

    #3 feat: chat MVC 패턴 생성

---
+++ src/main/java/com/psr/chat/chat/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.psr.chat.chat.controller;
+
+import com.psr.chat.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chats")
+public class ChatController {
+
+    private final ChatService chatService;
+
+}
