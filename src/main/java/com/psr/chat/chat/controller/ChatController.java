81a0e11411e87683b7aabfd8021fb7dd6252114c
sojungpp <sophie2458@naver.com>
2023-09-26 15:28:59 +0900

    #5 feat: publisher 구현

--- src/main/java/com/psr/chat/chat/controller/ChatController.java
+++ src/main/java/com/psr/chat/chat/controller/ChatController.java
@@ -1,15 +1,22 @@
 package com.psr.chat.chat.controller;
 
-import com.psr.chat.chat.service.ChatService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.psr.chat.chat.entity.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
 @RequiredArgsConstructor
-@RequestMapping("/chats")
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    // /pub/chat/message 로 발행 요청 -> /sub/chat/room/{roomId} 로 메시지 보내기
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getChatRoom().getId(), message);
+    }
 
 }
